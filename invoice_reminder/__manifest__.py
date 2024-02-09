2b13ea7a9da19eb417f58cbee1d7ba23b44d003e
enzohp <159511386+enzohp@users.noreply.github.com>
2024-02-08 22:39:33 -0800

    Added files via upload
    Creates init, manifest, email template, and scheduled action to send email reminder 7 days after invoice was due

---
+++ invoice_reminder/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Invoice Reminder',
+    'version': '17.0.0.0',
+    'summary': 'Automatic reminder for unpaid invoices 7 days after due date has passed',
+    'depends': ['account'],
+    'data': [
+        'data/cron_invoice_reminder.xml',
+        'data/invoice_reminder_email_template.xml',
+    ],
+    'installable': True,
+}
