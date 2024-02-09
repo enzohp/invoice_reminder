ff5415df2a25a099d1be798da4297e28f61e8f2d
enzohp <159511386+enzohp@users.noreply.github.com>
2024-02-08 21:09:54 -0800

    Add created files via upload
    Init, manifest, email template, and scheduled action

---
+++ invoice_reminder/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Invoice Reminder',
+    'version': '16.0.1.0',
+    'summary': 'Automatic reminder for unpaid invoices 7 days after due date has passed',
+    'depends': ['account'],
+    'data': [
+        'data/cron_invoice_reminder.xml',
+        'data/invoice_reminder_email_template.xml',
+    ],
+}
